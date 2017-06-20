646a3e3cba372ea942a0354e57a85bfc7ebe5793
Quezler <Quezler@me.com>
2017-06-20 12:54:38 +0000

    Apply fixes from StyleCI

--- src/Enums/Pagina.php
+++ src/Enums/Pagina.php
@@ -6,5 +6,5 @@ class Pagina extends \SplEnum
 {
     const base = 'https://bankieren.mijn.ing.nl';
 
-    const betalen = self::base . '/particulier/betalen/index';
+    const betalen = self::base.'/particulier/betalen/index';
 }
